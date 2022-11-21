ea7764f53631c2da2c76e98b7974644e788f8235
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-21 19:09:58 -0300

    first commit

---
+++ exercicio3.php
@@ -0,0 +1,10 @@
+<?php
+$n = $_POST['numero'];
+    if($n > 0){
+        echo "O número ".$n." é positivo";
+            }else if($n == 0) {
+                echo "O número ".$n." é igual a zero";
+                    }else {
+                        echo "O número ".$n." é negativo"; 
+                        }
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title> Exercício 3 Quiz </title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <div class="container">
+        <form action="exercicio3.php" method="POST">
+            <div id="til">
+                <h1> Exercício 3 Quiz</h1>
+            </div>
+            <p id="p">
+                Insira um número: <input type="number" name="numero" />
+            </p>
+            <div id="bt">
+                <input type="submit" value="Resultado" name='res' id="res">
+            </div>
+    </div>
+    </form>
+</body>
+</html>
